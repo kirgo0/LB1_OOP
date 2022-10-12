f0eea46eebbedf3d657a5e2ccfef3b1952f08def
kirgo <kirgotop@gmail.com>
2022-10-12 16:24:42 +0300

    Second Commit

--- Lb1/SomeGame.cs
+++ Lb1/SomeGame.cs
@@ -5,14 +5,15 @@ namespace Lb1
     public enum Status { Win, Lose }
     public class SomeGame
     {
-        private Random random = new Random();
+        private Random _random;
         public static int matchIndex { set; get; }
         
         private GameAccount p1, p2;
         
         public SomeGame(GameAccount p1, GameAccount p2)
         {
-            matchIndex = new Random().Next(0,100);
+            this._random = new Random();
+            matchIndex = _random.Next(0,100);
             this.p1 = p1;
             this.p2 = p2;
         }
@@ -20,11 +21,10 @@ namespace Lb1
         public void CreateMatch()
         {
             matchIndex++;
-            int matchRating = random.Next(5,25);
+            int matchRating = _random.Next(5,25);
             if (matchRating < 0) throw new Exception("Rating cannot be less then 0");
-            if (random.Next(0,10) > 5)
+            if (_random.Next(0,10) > 5)
             {
-                Console.WriteLine();
                 p1.WinGame(p2.UserName,matchRating);
                 p2.LoseGame(p1.UserName,matchRating);
             }
